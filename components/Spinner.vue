70a554b3c0eb30a29879784050b57374a94e7cff
martenweijer <martenweijer@gmail.com>
2021-05-02 12:31:30 +0200

    #1 authentication

---
+++ components/Spinner.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-overlay opacity="0.9">
+    <v-card light>
+      <v-card-text>
+        <v-progress-circular
+          size="100"
+          color="primary"
+          indeterminate
+          ></v-progress-circular>
+      </v-card-text>
+    </v-card>
+  </v-overlay>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-text>
+          {{account}}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState(['account']),
+}
+</script>
